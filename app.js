6e7b75c45e017c872d4b6c69120d087c9317f935
Nick Parsley <nicholasparsley@gmail.com>
2020-05-14 04:44:47 +0000

    formatting budget numbers

--- app.js
+++ app.js
@@ -148,6 +148,32 @@ var UIController = (function() {
 
   };
 
+  var formatNumber = function(num, type) {
+    var numSplit, int, dec;
+    /*
+    + or - before number
+    exactly 2 decimal points
+    comma separating the thousands
+    
+    2310.4567 -> + 2,310.46
+    2000 -> 2,000.00
+    */
+
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split('.');
+
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0, int.length -3) + ',' + int.substr(int.length -3, 3);  // input 23510, output 23,510
+    }
+
+    dec = numSplit[1];
+
+    return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+
+  };
   return {
     getInput: function() {
       return {
@@ -199,9 +225,12 @@ var UIController = (function() {
     },
 
     displayBudget: function(obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+      document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
       document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
 
       if(obj.percentage > 0) {
