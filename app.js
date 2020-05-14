2ae012af714692a54b2e1578bf52670cae926002
Nick Parsley <nicholasparsley@gmail.com>
2020-05-14 06:40:46 +0000

    ui update

--- app.js
+++ app.js
@@ -175,6 +175,13 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
 
   };
+
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++){
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -244,11 +251,6 @@ var UIController = (function() {
 
     displayPercentages: function(percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++){
-          callback(list[i], i);
-        }
-      };
 
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
@@ -270,6 +272,21 @@ var UIController = (function() {
 
       year = now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue);
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputButton).classList.toggle('red');
+
     },
 
     getDOMstrings: function() {
@@ -295,6 +312,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
